6bc99c048f2bff27cd75cdb77baf6b92de0bfe63
douglas254 <dobara@kabarak.ac.ke>
2021-02-24 17:02:27 +0300

    header

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+<head><title></title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <header class="header">
+            <div class="logo">
+                <a href="#"><img src="logo (1).png" alt="adda"><a/>
+            </div>
+                
+            <nav class="navbar">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Services</a></li>
+                    <li><a href="">Contact us</a></li>
+                    <button type="button" class="login-btn">Log In</button>
+                </ul>
+            </nav>
+                    
+        </header>
+    </div>
+</body>
+</html>
